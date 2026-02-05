f0ee32dea185a6149026efe04bf872e9dea674cb
Bethireddy Kesava Nagendra Reddy <150110071+kesavanagendrareddy@users.noreply.github.com>
2026-02-05 11:57:09 +0530

    Update Django settings with placeholders for email

--- truedge/truedge/settings.py
+++ truedge/truedge/settings.py
@@ -139,5 +139,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'placementcourse112@gmail.com' # Replace with your email
-EMAIL_HOST_PASSWORD = 'njzypywghptymzhy' # Replace with your password
+EMAIL_HOST_USER = 'Enter your email' # Replace with your email
+EMAIL_HOST_PASSWORD = 'Enter your Password' # Replace with your password
+
