6ce24ff6df6f683def6ec42ae5d73f5354a053c7
Bethireddy Kesava Nagendra Reddy <150110071+kesavanagendrareddy@users.noreply.github.com>
2026-02-06 13:34:42 +0530

    Update Django settings with email configuration

--- truedge/truedge/settings.py
+++ truedge/truedge/settings.py
@@ -139,5 +139,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'placementcourse112@gmail.com' # Replace with your email
-EMAIL_HOST_PASSWORD = 'njzypywghptymzhy' # Replace with your password
+EMAIL_HOST_USER = 'your email goes here' # Replace with your email
+EMAIL_HOST_PASSWORD = 'your password goes here' # Replace with your password
+
